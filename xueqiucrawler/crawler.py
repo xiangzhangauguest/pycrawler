7c86069bd325d0130c3713f04ce94c4325cd9a58
zhangxiangjor <zhangxiang0831@gmail.com>
2017-01-09 18:51:40 +0800

    change url

--- xueqiucrawler/crawler.py
+++ xueqiucrawler/crawler.py
@@ -1,12 +1,12 @@
 import requests
 
-url = 'https://xueqiu.com/statuses/search.json?count=10&comment=0&symbol=WB&hl=0&source=all&sort=alpha&page=1&_=1483888712749'
+url = 'https://xueqiu.com/statuses/search.json?count=10&comment=0&symbol=WB&hl=0&source=all&sort=time&page=2&_=1483939336621'
 headers = {
-	'User-Agent': '"Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:39.0) Gecko/20100101 Firefox/39.0"',
+	'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36',
 	'Host': 'xueqiu.com',
 	'Accept': 'application/json, text/javascript, */*; q=0.01',
 	'Referer': 'https://xueqiu.com/S/WB',
-	'Cookie': 'Hm_lvt_1db88642e346389874251b5a1eded6e3=1483793124,1483844385,1483878869,1483878874; Hm_lpvt_1db88642e346389874251b5a1eded6e3=1483889168; s=7i17flg74n; xq_a_token=660255dff38226bba32536d4e308cdbc3399aafe; xq_r_token=9d1de96fdd5ebd54732f300ef44f779ca6e5716f; u=71483868100216; __utma=1.675574121.1483878872.1483884676.1483887568.4; __utmc=1; __utmz=1.1483878872.1.1.utmcsr=baidu|utmccn=(organic)|utmcmd=organic; __utmb=1.15.10.1483887568; __utmt=1'
+    'Cookie': 's=5r11gm4726; xq_r_token=e386c2c5442603d86294a718b13da436909b3f07; u=341483007204349; _sid=qaxE4GIPGdismx6xBWl5EHgKDa1WXh; xq_a_token=fa4b993b49cc434e60f8eed7fd93599969c092c1; xq_is_login=1; bid=f7e919156871e526c88cc617624b4f25_ixa9haoo; webp=0; Hm_lvt_1db88642e346389874251b5a1eded6e3=1483007469,1483009225,1483083745,1483094424; Hm_lpvt_1db88642e346389874251b5a1eded6e3=1483939336; __utmt=1; __utma=1.755614866.1482394030.1483934576.1483939336.9; __utmb=1.1.10.1483939336; __utmc=1; __utmz=1.1483083745.5.2.utmcsr=baidu|utmccn=(organic)|utmcmd=organic'
 }
 
 html = requests.get(url, headers=headers)
