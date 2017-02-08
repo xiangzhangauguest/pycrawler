d1f29dd09ad0c4716997c731e9736ae3b2b5082c
zhangxiangjor <zhangxiang0831@gmail.com>
2017-02-08 18:58:54 +0800

    add total count

--- xueqiucrawler/crawler.py
+++ xueqiucrawler/crawler.py
@@ -13,13 +13,14 @@ import jieba.analyse
 from bosonnlp import BosonNLP
 
 def crawler(s_name, page):
-    url = 'https://xueqiu.com/statuses/search.json?count=10&comment=0&symbol={s_name}&hl=0&source=all&sort=time&page={page}&_=1483939336621'.format(s_name=s_name, page=page)
+    url = 'https://xueqiu.com/statuses/search.json?count=10&comment=0&symbol={s_name}&hl=0&source=all&sort=time&page={page}&_=1486548896521'.format(s_name=s_name, page=page)
     headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36',
         'Host': 'xueqiu.com',
         'Accept': 'application/json, text/javascript, */*; q=0.01',
         'Referer': 'https://xueqiu.com/S/{s_name}'.format(s_name=s_name),
-        'Cookie': 's=5r11gm4726; xq_r_token=e386c2c5442603d86294a718b13da436909b3f07; u=341483007204349; _sid=qaxE4GIPGdismx6xBWl5EHgKDa1WXh; xq_a_token=fa4b993b49cc434e60f8eed7fd93599969c092c1; xq_is_login=1; bid=f7e919156871e526c88cc617624b4f25_ixa9haoo; webp=0; Hm_lvt_1db88642e346389874251b5a1eded6e3=1483007469,1483009225,1483083745,1483094424; Hm_lpvt_1db88642e346389874251b5a1eded6e3=1483939336; __utmt=1; __utma=1.755614866.1482394030.1483934576.1483939336.9; __utmb=1.1.10.1483939336; __utmc=1; __utmz=1.1483083745.5.2.utmcsr=baidu|utmccn=(organic)|utmcmd=organic'
+        'Connection': 'keep-alive',
+        'Cookie': 's=5u11qd0x09; xq_a_token=4939ec38314fd03eb90e7d93a689a274dfb6487e; xq_r_token=f9162a16f515c6e1105bb2d2366276607af6e5f0; u=941486551063854; Hm_lvt_1db88642e346389874251b5a1eded6e3=1486551276; Hm_lpvt_1db88642e346389874251b5a1eded6e3=1486551279; __utmt=1; __utma=1.520347875.1486551279.1486551279.1486551279.1; __utmb=1.1.10.1486551279; __utmc=1; __utmz=1.1486551279.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none)'
     }
 
     html = requests.get(url, headers=headers)
@@ -35,7 +36,7 @@ class Recorder:
     def init_rec_file(self, target_f):
         self.target_f = target_f
         with open(target_f, 'wb') as f:
-            keywords = [x.encode('GBK') for x in self.keywords]
+            keywords = [x.encode('GBK') for x in self.keywords] + ['总量'.decode('utf8').encode('GBK')]
             f.write('date,' + ','.join(keywords) + '\n')
 
     def record(self, content, start_time, end_time):
@@ -45,6 +46,7 @@ class Recorder:
             for keyword in self.keywords:
                 freq = content[keyword]
                 f.write(',{freq}'.format(freq=freq))
+            f.write(',{total}'.format(total=content['total']))
             f.write("\n")
         
 if __name__ == '__main__':
@@ -158,6 +160,7 @@ if __name__ == '__main__':
                     seg = jieba.cut(text, cut_all=False)
                     for word in seg:
                         fenci_res[word] += 1
+                    fenci_res['total'] += 1
                     
                     """
                     # tag
